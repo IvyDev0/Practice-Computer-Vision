64b222b397fc3b289be60f31710016ea4a6369fe
YiyiDeng <ivy112014@outlook.com>
2017-04-06 15:16:11 +0800

    update！

--- p1.m
+++ p1.m
@@ -1,7 +1,34 @@
 clear; clc
-% 15组训练集
+% 对 15x7个训练样本做 PCA 提取特征
 for k = 1:15
-    for i = 1:11
+    for i = 1:7
+        img = imread(['p1_data/00', num2str(k), '/0', num2str(i), '.jpg']);
+        array = im2double(rgb2gray(img));
+        [m, n] = size(array);
+        Sample( (k-1)*7+i,: ) = reshape(array, 1, m*n);
+    end
+    Avarage(k,:) = mean(Sample((k-1)*7+1:k*7,:),1);  % 得到每个组训练样本的平均脸
+end 
+[base, SCORE, latent] = pca(Sample); 
+
+
+% 去方差占比前90%，进一步降维
+weight = latent(:) / sum(latent);
+sumw = 0;
+for i = 1:size(latent)
+    sumw = sumw+weight(i);
+    if sumw > 0.9
+        dimension = i;
+        break
+    end
+end
+base = base(:,1:dimension);
+PAvarage = Avarage * base;  % 将15个组的平均脸降维
+
+% 对 15x4个测试样本
+correct = 0;
+for k = 1:15
+    for i = 8:11
         if i >= 10
             img = imread(['p1_data/00', num2str(k), '/', num2str(i), '.jpg']);
         else
@@ -9,35 +36,17 @@ for k = 1:15
         end
         array = im2double(rgb2gray(img));
         [m, n] = size(array);
-        % Data 每一行为一个图像的灰度值向量
-        Data(i,1:m*n,k) = reshape(array, 1, m*n);
-    end
-    Sample(:,1:m*n,k) = Data(1:7,1:m*n,k);
-    Test(:,1:m*n,k) = Data(8:11,1:m*n,k);
-    
-    % 用训练样本做PCA分析
-    % 得到正交变换的基，和特征脸
-    base(1:m*n, :, k) = pca(Sample(:, :, k)); 
-    D(:,:,k) = Sample(:,:,k) * base(:,:,k);    
-end
-
-% Test
-for k = 1:15
-    correct = 0;
-    for i = 8:11        
-        test = Test(i-7,:,k) * base(:,:,k); % 得到测试样本的特征脸
-        for j = 1:15 
-            % 计算测试与每组训练样本之间的特征脸的距离
-            for m = 1:7
-                Dis(m,:,j) = D(m,:,j)-test;
-            end
-            distance(j) = norm(Dis(:,:,j),inf);
-        end
-        % 看是和哪一组特征脸的距离最小
-        if find(distance==min(distance)) == k  
+        Ptest = reshape(array, 1, m*n) * base;
+        % 计算15个组的特征脸和该降维后的测试特征脸的欧式距离
+        % 将PAvarage的每一行减去Ptest
+        Dis = PAvarage - repmat(Ptest, size(PAvarage,1) ,1);
+        Dis = sum(Dis.^2,2);
+        if find(Dis==min(Dis)) == k 
             correct = correct+1;
-        end        
+        end 
     end
-    ratio(k) = correct / 4; % 该组的识别率
-end
-acc = sum(ratio) / 15 % 15组的平均识别率
+end     
+acc = correct / (15*4)
+    
+    
+
